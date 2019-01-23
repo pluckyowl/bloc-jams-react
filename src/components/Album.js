29438f6d8b0977b4a87ac72f34e92ea9897e6e3c
Meghan Vidal <meghan.vidal@yahoo.com>
2019-01-23 15:17:28 -0800

    Assignment 6 Components: Album: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
 
 class Album extends Component {
@@ -32,6 +33,16 @@ class Album extends Component {
              <col id="song-duration-column" />
            </colgroup>
            <tbody>
+             {
+               this.state.album.songs.map( (song, index) =>
+               <tr key={index}>
+                 <td className="song-number">{index+1}</td>
+                 <td className="song-title">{song.title}</td>
+                 <td className="song-duration">{song.duration}</td>
+               </tr>
+               )
+             }
+
            </tbody>
          </table>
        </section>
